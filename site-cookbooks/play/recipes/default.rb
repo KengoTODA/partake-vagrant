d0edfcaa90d60d9afdde3ada1fcea43fcc5554a3
Kengo TODA <skypencil@gmail.com>
2013-04-05 09:37:04 +0800

    moved play cookbook into /site-cookbooks

---
+++ site-cookbooks/play/recipes/default.rb
@@ -0,0 +1,23 @@
+# default.rb
+#
+
+package "zip"
+
+execute "install-play" do
+    user "vagrant"
+    cwd node['play']['install_dir']
+    command <<-EOH
+    unzip play-#{node[:play][:version]}.zip
+    sudo ln -s /home/vagrant/play-#{node[:play][:version]} /usr/local/bin/play
+    EOH
+    action :nothing
+end
+
+remote_file "#{node[:play][:install_dir]}/play-#{node[:play][:version]}.zip" do
+    user "vagrant"
+    source "http://download.playframework.org/releases/play-#{node[:play][:version]}.zip"
+    mode "0644"
+    notifies :run, "execute[install-play]", :immediately
+    action :create_if_missing
+end
+
